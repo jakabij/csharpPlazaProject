5ec6b01a8aa770034cb5eec66f8474b77be98ab7
jakabij <jakabistvanj@gmail.com>
2020-02-14 15:18:51 +0100

    Init Plaza interface

---
+++ PlazaProject/Interface1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlazaProject
+{
+    interface Plaza
+    {
+        public List<Shop> GetShops();
+        public void AddShop(Shop shop);
+        public void RemoveShop(Shop shop);
+        public Shop FindShopByName(string name);
+        public bool IsOpen();
+        public void Open();
+        public void Close();
+
+    }
+}
